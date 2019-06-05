8d592dbc20e939113d5bf11dafc4dcc40f68004a
gaoe6789 <50209748+gaoe6789@users.noreply.github.com>
2019-06-05 09:56:31 -0700

    Update mathgames.c

--- mathgames.c
+++ mathgames.c
@@ -1,28 +1,30 @@
- #include <stdio.h>
+
+
+   #include <stdio.h>
    #include <stdlib.h>
    int main (void)
 
     {
     int i;
-    int response;
+    int number;
     float usrpercentage;
     float percentage = 0.0;
-    int correctAnswers = 0;
-    int incorrectAnswers = 0;
+    int correct = 0;
+    int incorrect = 0;
 
-    printf("\nMath Quiz\n");
-    printf("Please enter # of problems you would wish to try:");
-    scanf("%d", &response);
+    printf("\nMath Game\n");
+    printf("Please enter # of problems you want to answer:");
+    scanf("%d", &number);
     printf("Please enter the minimum percentage of the problems you want to get correct: ");
     scanf("%f", &usrpercentage);
-    if(response == 0)
+    if(number == 0)
     {
-        printf("\nThanks for playing!\n");
+        printf("\nBye!\n");
         return 0;
     }
 while (percentage<usrpercentage)
 {
-    for(i=0; i<response; i++)
+    for(i=0; i<number; i++)
     {
         int answer = 0;
         int a = rand() % 12;
@@ -30,26 +32,25 @@ while (percentage<usrpercentage)
         printf("\n%d * %d = ",a ,b);
         scanf("%d", &answer);
         if((a * b) == answer){
-            printf("\nCongratulations You are correct!\n");
-            correctAnswers++;
+            printf("\Yay You are correct!\n");
+            correct++;
         }
         else{
             printf("Sorry you were incorrect!\n");
-            incorrectAnswers++;
+            incorrect++;
         }
           }
     
-    printf("\n\nYour Results:\n\n\n");
-    printf("Number Incorrect: %d\n", incorrectAnswers);
-    printf("Number Correct: %d\n", correctAnswers);
-    if(100*correctAnswers/response >= usrpercentage){
-        printf("You Passed!\nGood work!\n\n");
+    printf("\n\nResults:\n\n\n");
+    printf("Number Incorrect: %d\n", incorrect);
+    printf("Number Correct: %d\n", correct);
+    if(100*correct/number >= usrpercentage){
+        printf("You got your percentage!\nGood work!\n\n");
     }
     else{
         printf("You did not pass!\nYou need more work!\n\n");
     }  
-    percentage = 100*correctAnswers/response;
-    printf("%f", percentage);
+    percentage = 100*correct/number;
 
 }
 
